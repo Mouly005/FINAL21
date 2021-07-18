59213167ed5f9d1930083c1aaa87f7f9b4b5397a
Mouly005 <84656123+Mouly005@users.noreply.github.com>
2021-07-18 18:29:14 +0600

    lab2

---
+++ lab task2/dashboard.php
@@ -0,0 +1,11 @@
+<html>
+	<body>
+	<center>
+	<h1>  Dashboard </h1>
+	
+	<a target="_blank" href="allstudent.php"> All Student </a>
+	 <a target="_blank" href="addstudent.php"> Add Student </a>
+	 </center>
+	
+	</body>
+</html>	
